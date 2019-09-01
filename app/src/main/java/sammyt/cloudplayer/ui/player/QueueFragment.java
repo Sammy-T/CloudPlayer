8ec62b642756d571f37a899d8c609875e346d834
Sammy-T <cyam9019@aim.com>
2019-09-01 14:36:49 -0400

    Set up Queue layout

---
+++ app/src/main/java/sammyt/cloudplayer/ui/player/QueueFragment.java
@@ -0,0 +1,27 @@
+package sammyt.cloudplayer.ui.player;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import sammyt.cloudplayer.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class QueueFragment extends Fragment {
+
+    public QueueFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_queue, container, false);
+    }
+}
